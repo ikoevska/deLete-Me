402f43361fbebe1cab0ef3b2e386d7cd88a1c326
Iva Koevska <iva.koevska@telerik.com>
2016-02-04 09:47:03 +0000

    Changes made by Iva Koevska (iva.koevska@telerik.com).

---
+++ clone-extensibility-with-gens/components/nachos/index.js
@@ -0,0 +1,113 @@
+'use strict';
+
+app.nachos = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_nachos
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_nachos
+(function(parent) {
+    var dataProvider = app.data.cloneExtensibilityWithGens,
+        processImage = function(img) {
+            if (!img) {
+                var empty1x1png = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQI12NgYAAAAAMAASDVlMcAAAAASUVORK5CYII=';
+                img = 'data:image/png;base64,' + empty1x1png;
+            } else if (img.slice(0, 4) !== 'http' &&
+                img.slice(0, 2) !== '//' && img.slice(0, 5) !== 'data:') {
+                var setup = dataProvider.setup || {};
+                img = setup.scheme + ':' + setup.url + setup.appId + '/Files/' + img + '/Download';
+            }
+
+            return img;
+        },
+        flattenLocationProperties = function(dataItem) {
+            var propName, propValue,
+                isLocation = function(value) {
+                    return propValue && typeof propValue === 'object' &&
+                        propValue.longitude && propValue.latitude;
+                };
+
+            for (propName in dataItem) {
+                if (dataItem.hasOwnProperty(propName)) {
+                    propValue = dataItem[propName];
+                    if (isLocation(propValue)) {
+                        dataItem[propName] =
+                            kendo.format('Latitude: {0}, Longitude: {1}',
+                                propValue.latitude, propValue.longitude);
+                    }
+                }
+            }
+        },
+        dataSourceOptions = {
+            type: 'everlive',
+            transport: {
+                typeName: 'Comments',
+                dataProvider: dataProvider
+            },
+
+            change: function(e) {
+                var data = this.data();
+                for (var i = 0; i < data.length; i++) {
+                    var dataItem = data[i];
+
+                    flattenLocationProperties(dataItem);
+                }
+            },
+            schema: {
+                model: {
+                    fields: {
+                        'UserId': {
+                            field: 'UserId',
+                            defaultValue: ''
+                        },
+                        'ActivityId': {
+                            field: 'ActivityId',
+                            defaultValue: ''
+                        },
+                    },
+                    icon: function() {
+                        var i = 'globe';
+                        return kendo.format('km-icon km-{0}', i);
+                    }
+                }
+            },
+            serverFiltering: true,
+            serverSorting: true,
+            serverPaging: true,
+            pageSize: 50
+        },
+        dataSource = new kendo.data.DataSource(dataSourceOptions),
+        nachosModel = kendo.observable({
+            dataSource: dataSource,
+            itemClick: function(e) {
+                app.mobileApp.navigate('#components/nachos/details.html?uid=' + e.dataItem.uid);
+            },
+            detailsShow: function(e) {
+                var item = e.view.params.uid,
+                    dataSource = nachosModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(item);
+                itemModel.ActivityIdUrl = processImage(itemModel.ActivityId);
+                if (!itemModel.ActivityId) {
+                    itemModel.ActivityId = String.fromCharCode(160);
+                }
+                nachosModel.set('currentItem', itemModel);
+            },
+            currentItem: null
+        });
+
+    if (typeof dataProvider.sbProviderReady === 'function') {
+        dataProvider.sbProviderReady(function dl_sbProviderReady() {
+            parent.set('nachosModel', nachosModel);
+        });
+    } else {
+        parent.set('nachosModel', nachosModel);
+    }
+})(app.nachos);
+
+// START_CUSTOM_CODE_nachosModel
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_nachosModel
